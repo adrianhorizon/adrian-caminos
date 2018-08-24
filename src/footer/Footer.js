cf97483378a97ace777d4d6ecc158e407d530d66
Adrian Camilo Parra Caminos <adriancaminos@sqdm.com>
2018-08-24 17:49:01 -0500

    cambio en el footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return(
+            <div className="Footer">
+                <footer className="Footer-header">
+                    <h1 className="Footer-title">Copyrigth 2018</h1>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Section from '../section/Section';
-
+import Footer from '../footer/Footer';
 class Home extends Component {
   render() {
+    const name = 'Adrían Camilo Parra Caminos';
     return (
       <div className="Home">
         <header className="Home-header">
-          <h1 className="Home-title">Adrian Camilo Parra Caminos</h1>
+          <h1 className="Home-title">{name}</h1>
         </header>
           <Section />
+            <Footer /> 
       </div>
     );
   }
