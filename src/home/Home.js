c91e53aff1d2d9e706cfb123f9e166ba9f88b8ee
Adrian Camilo Parra Caminos <adriancaminos@sqdm.com>
2018-08-30 17:55:15 -0500

    cambio new navbar

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import './Home.css'
+import PropTypes from 'prop-types'
+import Navigation from '../navigation/Navigation'
 import Section from '../section/Section'
 import Work from '../work/Work'
 import Footer from '../footer/Footer'
 import Contact from '../contact/Contact'
-import PropTypes from 'prop-types'
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+      <Navigation />
         <header className="Home-header">
           <h1 className="Home-title">{ this.props.name }</h1>
           <p>{this.props.text}</p>
--- src/section/Section.js
+++ src/section/Section.js
@@ -13,72 +13,72 @@ class Section extends Component {
       <div className="Section">
         <h1 className="Section-title">Skills</h1>
         <div className="Section-header">
+            <div className="Section-cell">
+            <i className="fas fa-file-code fa-2x"></i>
+              <h5>Architecture REST</h5>
+          </div>
+            <div className="Section-cell">
+              <i className="fab fa-aws fa-2x"></i>
+              <h5>Aws essentials</h5>
+          </div>
           <div className="Section-cell">
-          <i className="fas fa-file-code fa-2x"></i>
-            <h5>Architecture REST</h5>
-        </div>
+          <img src={Ocean} alt={Ocean} className="icons-cloud"/>
+              <h5>Digital ocean</h5>
+          </div>
           <div className="Section-cell">
-            <i className="fab fa-aws fa-2x"></i>
-            <h5>Aws essentials</h5>
+          <i className="fab fa-docker fa-2x"></i>
+            <h5>Docker</h5>
+          </div>
+          <div className="Section-cell">
+            <img src={Cloud} alt={Cloud} className="icons-cloud"/>
+            <h5>Google cloud</h5>
+          </div>
         </div>
-        <div className="Section-cell">
-        <img src={Ocean} alt={Ocean} className="icons-cloud"/>
-            <h5>Digital ocean</h5>
+        <div className="Section-header">
+          <div className="Section-cell">
+          <img src={Ionic} alt={Ionic} className="icons-ionic"/>
+            <h5>Ionic</h5>
+          </div>
+          <div className="Section-cell">
+            <img src={Firebase} alt={Firebase} className="icons"/>
+            <h5>Firebase</h5>
+          </div>
+          <div className="Section-cell">
+          <i className="fab fa-java fa-2x"></i>
+            <h5>Java/Spring</h5>
+          </div>
+          <div className="Section-cell">
+          <i className="fab fa-js fa-2x"></i>
+            <h5>JavaScript</h5>
+          </div>
+          <div className="Section-cell">
+          <img src={Webpack} alt={Webpack} className="icons-cloud"/>
+            <h5>Webpack</h5>
+          </div>
         </div>
-        <div className="Section-cell">
-        <i className="fab fa-docker fa-2x"></i>
-          <h5>Docker</h5>
-        </div>
-        <div className="Section-cell">
-          <img src={Cloud} alt={Cloud} className="icons-cloud"/>
-          <h5>Google cloud</h5>
-        </div>
-      </div>
-      <div className="Section-header">
-        <div className="Section-cell">
-        <img src={Ionic} alt={Ionic} className="icons-ionic"/>
-          <h5>Ionic</h5>
-        </div>
-        <div className="Section-cell">
-          <img src={Firebase} alt={Firebase} className="icons"/>
-          <h5>Firebase</h5>
-        </div>
-        <div className="Section-cell">
-        <i className="fab fa-java fa-2x"></i>
-          <h5>Java/Spring</h5>
-        </div>
-        <div className="Section-cell">
-        <i className="fab fa-js fa-2x"></i>
-          <h5>JavaScript</h5>
-        </div>
-        <div className="Section-cell">
-        <img src={Webpack} alt={Webpack} className="icons-cloud"/>
-          <h5>Webpack</h5>
-        </div>
-      </div>
-      <div className="Section-header">
-        <div className="Section-cell">
-          <i className="fab fa-angular fa-2x"></i>
-          <h5>Angular</h5>
-        </div>
-        <div className="Section-cell">
-        <i className="fab fa-react fa-2x"></i>
-          <h5>Reactjs</h5>
-        </div>
-        <div className="Section-cell">
+        <div className="Section-header">
+          <div className="Section-cell">
+            <i className="fab fa-angular fa-2x"></i>
+            <h5>Angular</h5>
+          </div>
+          <div className="Section-cell">
           <i className="fab fa-react fa-2x"></i>
-          <h5>React native</h5>
+            <h5>Reactjs</h5>
+          </div>
+          <div className="Section-cell">
+            <i className="fab fa-react fa-2x"></i>
+            <h5>React native</h5>
+          </div>
+          <div className="Section-cell">
+            <i className="fab fa-node-js fa-2x"></i>
+            <h5>NodeJS</h5>
+          </div>
+          <div className="Section-cell">
+          <img src={MongoDB} alt={MongoDB} className="icons-cloud"/>
+            <h5>MongoDB</h5>
+          </div>
         </div>
-        <div className="Section-cell">
-          <i className="fab fa-node-js fa-2x"></i>
-          <h5>NodeJS</h5>
-        </div>
-        <div className="Section-cell">
-        <img src={MongoDB} alt={MongoDB} className="icons-cloud"/>
-          <h5>MongoDB</h5>
-        </div>
-      </div>
-      <div className="Section-header">
+        <div className="Section-header">
         <div className="Section-cell">
         <i className="fab fa-github fa-2x"></i>
           <h5>Git</h5>
